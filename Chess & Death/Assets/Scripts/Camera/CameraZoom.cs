a41a2f11c6e2fd2ab56cef92d5333de84eeedfa9
MeatChips <84376178+MeatChips@users.noreply.github.com>
2022-05-17 13:31:33 +0200

    Old Unity Project files added
    - Scripts added
    - Sprites added
    - Folders added

---
+++ Chess & Death/Assets/Scripts/Camera/CameraZoom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    private Camera cam;
+
+    private float targetZoom;
+    private float yVelocity = 0.0f;
+    [SerializeField] private float zoomFactor = 50;
+    [SerializeField] private float zoomLerpSpeed = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        targetZoom = cam.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scrollData;
+        scrollData = Input.GetAxis("Mouse ScrollWheel");
+
+        targetZoom -= scrollData * zoomFactor;
+        targetZoom = Mathf.Clamp(targetZoom, 5f, 20f);
+        cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, targetZoom, ref yVelocity, Time.deltaTime * zoomLerpSpeed);
+    }
+}
